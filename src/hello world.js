1a884826b1c5de2bef2bc098376bbdfea2bfe9cc
Youssef-AbuTaleb <89806111+Youssef-AbuTaleb@users.noreply.github.com>
2022-06-30 00:28:27 +0200

    Add files via upload

---
+++ src/hello world.js
@@ -0,0 +1,4 @@
+function hello() {
+  console.log("hello Youssef");
+}
+export default hello;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import hello from "./hello world";
+hello();
+
+import "./style.css";
+
+import header from "./component/header/header";
+
+import "./component/bio/bio";
+// // // // nooo
+
+// import bio from "./component/bio/bio"
+import photo from "./assets/cover.jpg";
+
+// /*
+// this is a big comment
+//  */
